cd9a5c5a6b64483023f99890cd54665911352f49
LeBulldoge <alexey13031@gmail.com>
2019-07-11 02:05:07 +0300

    Added screenshots

--- nbody/Constants.h
+++ nbody/Constants.h
@@ -7,6 +7,6 @@ const double AU = 1.49597893e11;
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
-const int AMOUNT = 1000;
-const float THETA = 0.5f;
+const int AMOUNT = 10000;
+const float THETA = 1.f;
 const float PI = 3.14159265358979323846f;
